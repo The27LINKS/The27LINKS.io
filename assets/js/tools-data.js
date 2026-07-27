ad71721d1f13699f1f956ba77f78bedd43a64132
The 27LINKS Group Of Companies <the27linksofficial@gmail.com>
2026-07-27 19:08:07 +0530

    Update tools-data.js

--- assets/js/tools-data.js
+++ assets/js/tools-data.js
@@ -28,4 +28,3 @@ window.TOOL_ICONS = {
   seo: '<svg viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.8\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><circle cx=\"11\" cy=\"11\" r=\"8\"/><path d=\"m21 21-4.3-4.3\"/><path d=\"M8 11h6M11 8v6\"/></svg>',
  crab: '<svg viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"1.8\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><circle cx=\"11\" cy=\"11\" r=\"8\"/><path d=\"m21 21-4.3-4.3\"/><path d=\"M8 11h6M11 8v6\"/></svg>',
  };
-};
