8c3884d81e28ef6753ed51b5d1592f912740578b
The 27LINKS Group Of Companies <the27linksofficial@gmail.com>
2026-08-10 20:32:30 +0530

    Update loader.js

--- assets/posterStudio/js/loader.js
+++ assets/posterStudio/js/loader.js
@@ -6,11 +6,11 @@ async function fetchJSON(path) {
 }
 export async function loadAll() {
   const [templates, shapes, colors, fonts, defaults] = await Promise.all([
-    fetchJSON('../assets/posterStudio/data/templates.json'),
-    fetchJSON('../assets/posterStudio/data/shapes.json'),
-    fetchJSON('../assets/posterStudio/data/colors.json'),
-    fetchJSON('../assets/posterStudio/data/fonts.json'),
-    fetchJSON('../assets/posterStudio/data/defaults.json'),
+    fetchJSON('https://the27links.in/assets/posterStudio/data/templates.json'),
+    fetchJSON('https://the27links.in/assets/posterStudio/data/shapes.json'),
+    fetchJSON('https://the27links.in/assets/posterStudio/data/colors.json'),
+    fetchJSON('https://the27links.in/assets/posterStudio/data/fonts.json'),
+    fetchJSON('https://the27links.in/assets/posterStudio/data/defaults.json'),
   ]);
   return { templates, shapes, colors, fonts, defaults };
 }
